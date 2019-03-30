67d81b47f0c0e4dd66327c6cfa019b7588513e09
FROL256 <vova@frolov.pp.ru>
2019-03-30 15:01:31 +0300

    in progress with embree geom core

--- cpp/embree_connect.cpp
+++ cpp/embree_connect.cpp
@@ -36,12 +36,9 @@ extern "C" void gcore_destroy()
 extern "C" void gcore_init_and_clear()
 {
   gcore_destroy();
-
   g_data.m_meshes.resize(0);
   g_data.m_instances.resize(0);
-
   g_data.device = rtcNewDevice("");
-  
 }
 
 extern "C" int gcore_add_mesh_3f(const float* a_vertices3f, int a_vertexNum, const int* a_indices, int a_indicesNum)
@@ -59,7 +56,7 @@ extern "C" int gcore_add_mesh_3f(const float* a_vertices3f, int a_vertexNum, con
 
   if(triangles == nullptr)
   {
-    std::cout << "[c_gcore]: rtcSetNewGeometryBuffer failed to allocate memory" << std::endl;
+    std::cout << "[c_gcore]: rtcSetNewGeometryBuffer failed to allocate index memory" << std::endl;
     return 0;
   }
 
@@ -73,9 +70,18 @@ extern "C" int gcore_add_mesh_3f(const float* a_vertices3f, int a_vertexNum, con
     int B = a_indices[i*3 + 1]; 
     int C = a_indices[i*3 + 2];
 
-    if(A >= a_vertexNum) A = a_vertexNum-1;  // #NOTE: why this is happen ??? Check scenes !!!
-    if(B >= a_vertexNum) B = a_vertexNum-1;
-    if(C >= a_vertexNum) C = a_vertexNum-1;
+    if(A >= a_vertexNum || B >= a_vertexNum || C >= a_vertexNum) // #NOTE: why this is happen ??? Check scenes !!! 
+    {
+      A = a_vertexNum-1;   
+      B = a_vertexNum-1;
+      C = a_vertexNum-1;
+    }
+    else if(A < 0 || B < 0 || C < 0)                             // #NOTE: why this is happen ??? Check scenes !!! 
+    {
+      A = a_vertexNum-1;  
+      B = a_vertexNum-1;
+      C = a_vertexNum-1;
+    }
 
     if(A > maxVertexId) maxVertexId = A;
     if(B > maxVertexId) maxVertexId = B;
@@ -91,7 +97,7 @@ extern "C" int gcore_add_mesh_3f(const float* a_vertices3f, int a_vertexNum, con
   Vertex* vertices = (Vertex*)rtcSetNewGeometryBuffer(geom, RTC_BUFFER_TYPE_VERTEX, 0, RTC_FORMAT_FLOAT3, sizeof(Vertex), maxVertexId);
   if(vertices == nullptr)
   {
-    std::cout << "[c_gcore]: rtcSetNewGeometryBuffer failed to allocate memory" << std::endl;
+    std::cout << "[c_gcore]: rtcSetNewGeometryBuffer failed to allocate vertices memory" << std::endl;
     return 0;
   }
 
@@ -103,8 +109,10 @@ extern "C" int gcore_add_mesh_3f(const float* a_vertices3f, int a_vertexNum, con
   }
 
   rtcCommitGeometry(geom);
-  g_data.m_meshes.push_back( rtcNewScene(g_data.device) );
-  unsigned int geomID = rtcAttachGeometry(g_data.m_meshes[g_data.m_meshes.size()-1], geom); // geomID is always equal to 0
+  {
+    g_data.m_meshes.push_back( rtcNewScene(g_data.device) );
+    rtcAttachGeometry(g_data.m_meshes[g_data.m_meshes.size()-1], geom); // returned geomID is always equal to 0
+  }
   rtcReleaseGeometry(geom);
   
   return g_data.m_meshes.size()-1;
@@ -113,6 +121,33 @@ extern "C" int gcore_add_mesh_3f(const float* a_vertices3f, int a_vertexNum, con
 
 extern "C" int gcore_instance_meshes(int a_geomId, const float* a_matrices16f, int a_matrixNum)
 {
+  g_data.m_instances.resize(a_matrixNum);
+
+  for(size_t i=0; i < g_data.m_instances.size(); i++)
+  {
+    g_data.m_instances[i] = rtcNewGeometry (g_data.device, RTC_GEOMETRY_TYPE_INSTANCE);
+    rtcSetGeometryInstancedScene(g_data.m_instances[i], g_data.m_scene);
+    rtcSetGeometryTimeStepCount (g_data.m_instances[i], 1);
+    rtcAttachGeometry           (g_data.m_scene,        g_data.m_instances[i]);
+  }
+
+  // decrease fucking reference counter (embree3 equals shit)
+  //
+  for(size_t i=0; i < g_data.m_instances.size(); i++) 
+    rtcReleaseGeometry(g_data.m_instances[i]);
+
+  // set matrices
+  //
+  for(size_t i=0; i < g_data.m_instances.size(); i++)
+    rtcSetGeometryTransform(g_data.m_instances[i], 0, RTC_FORMAT_FLOAT4X4_ROW_MAJOR, a_matrices16f + 16*i);
+
+  // commit evetithing
+  //
+  for(size_t i=0; i < g_data.m_instances.size(); i++)
+    rtcCommitGeometry(g_data.m_instances[i]);
+
+  rtcCommitScene (g_data.m_scene);
+
   return 0;
 }
 
